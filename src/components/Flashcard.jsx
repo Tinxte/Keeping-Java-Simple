859622bdc0c3930e57a89eb9e9bfb5c15b268457
tinxte <150742495+Tinxte@users.noreply.github.com>
2024-09-22 23:09:00 -0500

    Card flips correctly, displaying answers on the back side

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,89 +1,88 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 
-export default function FlashCard({number, data}) {
+export default function FlashCard({ number, data }) {
+  //Pull starting index and flashcard data from parent component JSON
 
-//Pull starting index and flashcard data from parent component JSON
+  let startingCardIndex = number;
+  // console.log(startingCardIndex);
 
-let startingCardIndex = number;
-// console.log(startingCardIndex);
+  let trivia = data;
+//   console.log(trivia);
 
-let trivia = data;
-// console.log(trivia);
+  let currentCardIndex = startingCardIndex;
 
-let currentCardIndex = startingCardIndex;
-
-const [flashCardState, setFlashCardState] = useState({
+  //TODO refactor hook to only track index, not question/answer
+  const [flashCardState, setFlashCardState] = useState({
     currentCardState: "question",
-    currentStateIndex: currentCardIndex
-});
+    currentStateIndex: currentCardIndex,
+  });
 
-const [isFlipped, setIsFlipped] = useState(false);
-const [isAnimating, setIsAnimating] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
 
-
-// Flip animation handler
-function handleFlip() {
+  // Flip animation handler
+  function handleFlip() {
     if (!isAnimating) {
-        setIsFlipped(!isFlipped);
-        setIsAnimating(true);
+      setIsFlipped(!isFlipped);
+      setIsAnimating(true);
     }
-}
+  }
 
-// Change card contents handler
-function handleCardClick() {
+  // Change card contents handler
+  function handleCardClick() {
     if (flashCardState.currentCardState === "question") {
-        setFlashCardState({currentCardState: "answer",
-    currentStateIndex: flashCardState.currentStateIndex});
+      setFlashCardState({
+        currentCardState: "answer",
+        currentStateIndex: flashCardState.currentStateIndex,
+      });
+    } else if (flashCardState.currentCardState === "answer") {
+      setFlashCardState({
+        currentCardState: "question",
+        currentStateIndex: flashCardState.currentStateIndex,
+      });
     }
-    else if (flashCardState.currentCardState === "answer") {
-            setFlashCardState({currentCardState: "question",
-                currentStateIndex: flashCardState.currentStateIndex
-            })
+  }
+
+  function handleNextClick() {
+
+    if (flashCardState.currentStateIndex < trivia.length - 1) {
+      setFlashCardState({
+        currentCardState: "question",
+        currentStateIndex: flashCardState.currentStateIndex + 1,
+      });
+    } else {
+      setFlashCardState({
+        currentCardState: "question",
+        currentStateIndex: 0,
+      });
     }
-};
+  }
 
-    function handleNextClick() {
-        //TODO "next" button switches to next card trivia
+  return (
+    <>
+        <div className="flip-card" onClick={handleFlip}>
+          <motion.div
+            className="flip-card-inner"
+            initial="false"
+            animate={{ rotateY: isFlipped ? 180 : 360 }}
+            transition={{ duration: 0.6, animationDirection: "normal" }}
+            onAnimationComplete={() => setIsAnimating(false)}
+          >
+            {/* TODO refactor card and state change to sync with flip animation*/}
 
-        if (flashCardState.currentStateIndex < trivia.length-1)
-        {
-            setFlashCardState({
-                currentCardState: "question",
-                currentStateIndex: flashCardState.currentStateIndex + 1
-            })
-        }
-        else {
-            setFlashCardState({
-                currentCardState: "question",
-                currentStateIndex: 0
-            })        
-        }
-        
-    }
-
-//TODO Write return statement with FlashCard + Next button HTML
-
-    return (
-        <>
-            <div className="flip-card" onClick={handleFlip}>
-                <motion.div
-                className="flip-card-inner"
-                initial="false"
-                animate={{rotateY: isFlipped ? 180 : 360}}
-                transition={{duration: 0.6, animationDirection: "normal"}}
-                onAnimationComplete={() => setIsAnimating(false)}
-                >
-
-                <div id="flashCard" className={flashCardState.currentCardState} onClick={handleCardClick}>
-                <p>{trivia[flashCardState.currentStateIndex][flashCardState.currentCardState]}</p>
-                </div>
-
-                </motion.div>
-
+            <div className="flip-card-front">
+              <p>{trivia[flashCardState.currentStateIndex]["question"]}</p>
             </div>
-
-            <button className="nextButton" onClick={handleNextClick}>Next ➔</button>
-        </>
-    );
+            <div className="flip-card-back">
+              <p>{trivia[flashCardState.currentStateIndex]["answer"]}</p>
+            </div>
+          </motion.div>
+        </div>
+            {/* TODO correct absolute position issues so button displays in appropriate location */}
+      <button className="nextButton" onClick={handleNextClick}>
+        Next ➔
+      </button>
+    </>
+  );
 }
